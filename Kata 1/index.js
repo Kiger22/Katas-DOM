9bf5b1f234d5fb8f09c61f13c219b011116efdd8
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-02-26 23:44:31 +0100

    Update index.js

--- Kata 1/index.js
+++ Kata 1/index.js
@@ -1,39 +1,25 @@
-/* 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
-
-1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
-
-1.3 Usa querySelector para mostrar por consola todos los p
-
-1.4 Usa querySelector para mostrar por consola todos los elementos con 
-la clase.pokemon
-
-1.5 Usa querySelector para mostrar por consola todos los elementos con 
-el atributo data-function="testMe".
-
-1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
-data-function="testMe". */
-
-// 1.1
+// 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
 const botom = document.querySelector(".showme");
 console.log(botom);
 
-//1.2
+//1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
 const h1 = document.querySelector("#pillado");
 console.log(h1);
 
-//1.3
+//1.3 Usa querySelector para mostrar por consola todos los p
 const p = document.querySelectorAll("p");
 console.log(p);
 
-//1.4
+//1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+
 const pokemon = document.querySelectorAll(".pokemon");
 console.log(pokemon);
 
-//1.5
+//1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo data-function="testMe".
 const datFun = document.querySelectorAll("[data-function='testMe']");
 console.log(datFun);
 
-//1.6
+//1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo data-function="testMe".
 console.log(document.querySelectorAll("[data-function='testMe']",)[2]);
 
 
